96fe72df8ff901059d087bedc3c8b3255bfdaee1
Martin Genet <martin.genet@polytechnique.edu>
2018-09-28 10:02:19 +0200

    New Printer class.

--- print_tools.py
+++ print_tools.py
@@ -10,6 +10,7 @@
 ###                                                                  ###
 ########################################################################
 
+import os
 import sys
 
 ########################################################################
@@ -18,52 +19,220 @@ def my_print(
         verbose,
         string,
         newline=True,
-        flush=True):
+        flush=True): # MG20180416: Still used in myVTKPythonLibrary
 
     if not hasattr(my_print, "initialized"):
         my_print.initialized = True
         my_print.verbose_ini = verbose
     if (verbose > 0):
         sys.stdout.write((my_print.verbose_ini - verbose)*" |  "+string)
-        if (newline):
-            sys.stdout.write("\n")
-        if (flush):
-            sys.stdout.flush()
+        if (newline): sys.stdout.write("\n")
+        if (flush):   sys.stdout.flush()
 
 ########################################################################
 
-def print_str(string, tab=0, newline=True, flush=True):
+def print_str(string, tab=0, newline=True, flush=True): # MG20180416: Still used in dolfin_dic
     sys.stdout.write(" | "*tab + string)
     if (newline): sys.stdout.write("\n")
     if (flush):   sys.stdout.flush()
 
-def print_var(name, val, tab=0, newline=True, flush=True):
+def print_var(name, val, tab=0, newline=True, flush=True): # MG20180416: Still used in dolfin_dic
     sys.stdout.write(" | "*tab + name + " = " + str(val))
     if (newline): sys.stdout.write("\n")
     if (flush):   sys.stdout.flush()
 
-def print_sci(name, val, tab=0, newline=True, flush=True):
+def print_sci(name, val, tab=0, newline=True, flush=True): # MG20180416: Still used in dolfin_dic
     sys.stdout.write(" | "*tab + name.ljust(13) + " = " + format(val,".4e"))
     if (newline): sys.stdout.write("\n")
     if (flush):   sys.stdout.flush()
 
 ########################################################################
 
-#class Printer:
-    #def __init__(self):
-        #self.cur_level = 0
+class Printer:
 
-    #def inc(self):
-        #self.cur_level += 1
 
-    #def dec(self):
-        #self.cur_level -= 1
 
-    #def print_str(self, string, var_level=0):
-        #print  " | "*(self.cur_level+var_level) + string
+    def __init__(self,
+            tab=" |  ",
+            max_level=float("+Inf"),
+            filename=None,
+            silent=False):
 
-    #def print_var(self, name, val, var_level=0):
-        #print " | "*(self.cur_level+var_level) + name + " = " + str(val)
+        self.tab = tab
+        self.cur_level = 0
+        self.loc_level = 0
+        self.max_level = max_level
 
-    #def print_sci(self, name, val, var_level=0):
-        #print " | "*(self.cur_level+var_level) + name.ljust(13) + " = " + format(val,".4e")
+        if (silent):
+            self.output = open(os.devnull, "w")
+        else:
+            if (filename is None):
+                self.output = sys.stdout
+            else:
+                self.output = open(filename, "w")
+
+
+
+    def inc(self):
+        self.cur_level += 1
+
+
+
+    def dec(self):
+        self.cur_level -= 1
+
+
+
+    def print_str(self,
+            string,
+            var_level=0,
+            tab=True,
+            newline=True,
+            flush=True):
+
+        self.loc_level = self.cur_level + var_level
+        if (self.loc_level <= self.max_level):
+            if (tab):
+                self.output.write(self.loc_level*self.tab)
+            self.output.write(string)
+            if (newline):
+                self.output.write("\n")
+            if (flush):
+                self.output.flush()
+
+
+
+    def print_var(self,
+            name,
+            val,
+            var_level=0,
+            tab=True,
+            newline=True,
+            flush=True):
+
+        self.print_str(
+            string=name+" = "+str(val),
+            var_level=var_level,
+            tab=tab,
+            newline=newline,
+            flush=flush)
+
+
+
+    def print_sci(self,
+            name,
+            val,
+            var_level=0,
+            tab=True,
+            newline=True,
+            flush=True):
+
+        self.print_str(
+            string=name.ljust(13) + " = " + format(val,".4e"),
+            var_level=var_level,
+            tab=tab,
+            newline=newline,
+            flush=flush)
+
+
+
+    def close(self):
+
+        self.output.close()
+
+
+
+########################################################################
+
+class TablePrinter():
+
+
+
+    def __init__(self,
+            titles,
+            width=None,
+            filename=None,
+            silent=False):
+
+        if (silent):
+            self.output = open(os.devnull, "w")
+        else:
+            if (filename is None):
+                self.output = sys.stdout
+            else:
+                self.output = open(filename, "w")
+
+        self.titles = titles
+
+        if (width is None):
+            self.width = max([len(title) for title in self.titles])+2
+        else:
+            self.width = width
+
+        self.output.write("-"+"-".join(["-"*self.width for title in self.titles])+"-\n")
+        self.output.write("|"+"|".join([title.center(self.width) for title in self.titles])+"|\n")
+        self.output.write("-"+"-".join(["-"*self.width for title in self.titles])+"-\n")
+        self.output.flush()
+
+
+
+    def write_line(self,
+            values):
+
+        strings = []
+        for value in values:
+            if (len(str(value)) <= self.width):
+                strings += [str(value)]
+            else:
+                strings += [format(value, ".2e")]
+        self.output.write("|"+"|".join([string.center(self.width) for string in strings])+"|\n")
+        self.output.flush()
+
+
+
+    def close(self):
+
+        self.output.write("-"+"-".join(["-"*self.width for title in self.titles])+"-\n")
+        self.output.flush()
+
+        self.output.close()
+
+
+
+########################################################################
+
+class DataPrinter():
+
+
+
+    def __init__(self,
+            names,
+            filename,
+            width=None,
+            sep=" "):
+
+        self.names = names
+        self.filename = filename
+        if (width is None):
+            self.width = max([len(name) for name in self.names]+[18])+2
+        else:
+            self.width = width
+        self.sep = sep
+
+        self.file = open(self.filename, "w")
+        self.file.write("#"+self.sep.join([name.center(self.width) for name in self.names])+"\n")
+        self.file.flush()
+
+
+
+    def write_line(self,
+            values):
+
+        self.file.write(" "+self.sep.join([str(value).center(self.width) for value in values])+"\n")
+        self.file.flush()
+
+
+
+    def close(self):
+
+        self.file.close()
